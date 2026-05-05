34bbbb7e7e2c4a4f86dd0bb54a72686ee28d797c
x <x>
2026-05-05 22:00:46 +0300

    fixed font-weight to bold animation

--- app/components/home/Quality.vue
+++ app/components/home/Quality.vue
@@ -2,24 +2,18 @@
   import gsap from "gsap";
   import ScrollTrigger from "gsap/ScrollTrigger";
 
-  // BUG: This completely doesn't work, working on it
   onMounted(async () => {
     await nextTick();
 
     gsap.registerPlugin(ScrollTrigger);
 
-    // ScrollTrigger.getAll().forEach(t => t.kill())
-
     gsap.to(".focus", {
-      duration: 3,
-      "font-variation-settings": "'wght' 800",
-      "--font-weight": 800,
+      fontWeight: 900,
       scrollTrigger: {
         trigger: ".parent",
-        start: "top top",
+        start: "25% center",
         end: "+=500",
         scrub: 1,
-        pin: true,
       },
     });
   });
@@ -27,10 +21,10 @@
 
 <template>
   <div
-    class="parent /h-[40vh] xl: z-10 flex h-svh max-h-350 min-h-100 w-full items-center justify-center xl:min-h-300 xl:px-30"
+    class="h-[40vh] z-10 flex max-h-350 min-h-80 w-full items-center justify-center xl:min-h-300 xl:px-30"
   >
     <div
-      class="focus w-6/7 max-w-200 -translate-y-40 text-center font-[Inter] text-2xl text-4xl/ leading-[125%] md:max-w-400 xl:text-6xl 2xl:text-7xl"
+      class="focus w-6/7 max-w-200 text-center font-[Inter] font-[100] text-4xl leading-[125%] md:max-w-400 xl:text-7xl 2xl:text-8xl"
     >
       our company provides safe working conditions and provide quality work
       within reasonable timeframe
