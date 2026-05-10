abac2e745e12098feacae74906176b998286d62f
x <x>
2026-05-10 08:07:44 +0300

    fixing ..

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -4,10 +4,13 @@
   import ScrollTrigger from "gsap/ScrollTrigger";
   import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
+  const loaded = ref(false);
+
   const endSVG =
     "M-359 327 L397.71 109.629 C780.086 -0.211 1185.74 0.397 1567.78 111.383 L2310 327 V1940 H-359 V327Z";
 
   onMounted(async () => {
+    loaded.value = true;
     gsap.registerPlugin(SplitText);
     gsap.registerPlugin(ScrollTrigger);
     gsap.registerPlugin(MorphSVGPlugin);
@@ -76,78 +79,7 @@
   <div
     class="relative flex lg:min-h-[150svh] max-lg:max-h-100 w-full justify-center lg:overflow-visible"
   >
-    <div
-      class="absolute top-0 flex lg:h-max w-full flex-col items-center justify-center"
-    >
-      <div class="relative flex h-max w-full">
-        <img src="/back2.png" class="h-max w-full" />
-
-        <!-- WARNING: Please don't touch this div, it is for the blur -->
-        <div
-          class="inset-0/ bg-black/ /translate-y-45 absolute bottom-0 h-[55%] w-full"
-        >
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_0%,transparent_12.5%)] backdrop-blur-md"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_12.5%,transparent_25%)] backdrop-blur-[9px]"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_25%,transparent_37.5%)] backdrop-blur-[6px]"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_37.5%,transparent_50%)] backdrop-blur-[5px]"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_50%,transparent_62.5%)] backdrop-blur-[3px]"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_62.5%,transparent_75%)] backdrop-blur-[1.5px]"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_75%,transparent_87.5%)] backdrop-blur-[1px]"
-          ></div>
-          <div
-            class="absolute inset-0 mask-[linear-gradient(to_top,black_87.5%,transparent_100%)] backdrop-blur-[0.5px]"
-          ></div>
-        </div>
-      </div>
-
-      <div class="parent z-0 h-full w-full -translate-y-[16%]">
-        <svg
-          id="curve"
-          class="h-max w-full"
-          width="1920"
-          height="1940"
-          viewBox="0 0 1920 1940"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <!-- stroke="black" -->
-          <path
-            d="M397.571 217.852C780.038 327.718 1185.79 327.11 1567.92 216.098L2309.5 0.666016V1939.5H-358.5V0.663086L397.571 217.852Z"
-            fill="url(#paint0_linear_614_189)"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_614_189"
-              x1="975.5"
-              y1="354.106"
-              x2="975.5"
-              y2="1940"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#3E5CE1" />
-              <stop offset="0.729563" stop-color="#22327B" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-    </div>
-
-    <div class="bottom-0 flex lg:h-svh h-[40vh] w-full bg-black flex-col p-90"></div>
-
-    <div class="absolute z-0 flex lg:h-svh h-[40vh] w-full items-center justify-center">
+    <div class="absolute z-20 flex lg:h-svh h-[40vh] w-full items-center justify-center">
       <div
         class="heroheader pt-10/ flex flex-col items-center justify-center gap-6.5 p-6 text-black opacity-0"
       >
@@ -188,6 +120,76 @@
         </ActionBtn>
       </div>
     </div>
+
+    <div
+      class="absolute top-0 flex lg:h-max w-full flex-col items-center justify-center"
+    >
+      <div class="relative flex h-max w-full">
+        <img src="/back2.png" class="h-max w-full" />
+
+        <!-- WARNING: Please don't touch this div, it is for the blur -->
+        <div
+          class="inset-0/ bg-black/ /translate-y-45 absolute bottom-0 h-[55%] w-full"
+        >
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_0%,transparent_12.5%)] backdrop-blur-md"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_12.5%,transparent_25%)] backdrop-blur-[9px]"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_25%,transparent_37.5%)] backdrop-blur-[6px]"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_37.5%,transparent_50%)] backdrop-blur-[5px]"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_50%,transparent_62.5%)] backdrop-blur-[3px]"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_62.5%,transparent_75%)] backdrop-blur-[1.5px]"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_75%,transparent_87.5%)] backdrop-blur-[1px]"
+          ></div>
+          <div
+            class="absolute inset-0 mask-[linear-gradient(to_top,black_87.5%,transparent_100%)] backdrop-blur-[0.5px]"
+          ></div>
+        </div>
+      </div>
+
+      <div :class="loaded ? '' : 'hidden'" class="parent z-0 h-full w-full -translate-y-[16%]">
+        <svg
+          id="curve"
+          class="h-max w-full"
+          width="1920"
+          height="1940"
+          viewBox="0 0 1920 1940"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <!-- stroke="black" -->
+          <path
+            d="M397.571 217.852C780.038 327.718 1185.79 327.11 1567.92 216.098L2309.5 0.666016V1939.5H-358.5V0.663086L397.571 217.852Z"
+            fill="url(#paint0_linear_614_189)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear_614_189"
+              x1="975.5"
+              y1="354.106"
+              x2="975.5"
+              y2="1940"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#3E5CE1" />
+              <stop offset="0.729563" stop-color="#22327B" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+    </div>
+
 
     <div
       class="/max-h-350 /relative absolute -bottom-full z-10 flex h-svh w-full items-center justify-center"
